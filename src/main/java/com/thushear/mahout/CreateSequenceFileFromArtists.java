5b3b208c5d5305e5501b9ad0cfe4d3aecd26721e
kongming <kongming@jd.com>
2016-07-06 15:23:37 +0800

    m Signed-off-by: kongming <kongming@jd.com>

---
+++ src/main/java/com/thushear/mahout/CreateSequenceFileFromArtists.java
@@ -0,0 +1,66 @@
+package com.thushear.mahout;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.SequenceFile;
+import org.apache.hadoop.io.Text;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ * Created by kongming on 2016/7/6.
+ */
+public class CreateSequenceFileFromArtists {
+
+
+    public static void main(String[] args) throws IOException {
+        String inputPath = "data/lastfm/original/Lastfm-ArtistTags2007/artists.txt";
+        String outputPath =  "data/lastfm/sequencesfiles";
+        Path path = new Path(outputPath);
+
+        BufferedReader br = new BufferedReader(new FileReader(inputPath));
+        //creating Sequence Writerr
+        Configuration conf = new Configuration();
+
+        FileSystem fs = FileSystem.get(conf);
+        SequenceFile.Writer writer = new SequenceFile.Writer(fs,conf,path, LongWritable.class, Text.class);
+
+        String line = br.readLine();
+        String[] temp;
+        String tempValue = new String();
+        String delimiter = " ";
+        LongWritable key = new LongWritable();
+        Text value = new Text();
+        long tempkey = 0;
+        while (line != null){
+
+            line = br.readLine();
+            temp = line.split(delimiter);
+            value = new Text();
+            tempValue = "";
+            for (int i = 1;i<temp.length;i++) {
+                tempValue += temp[i] + delimiter;
+            }
+            value = new Text(tempValue);
+            System.out.println("writing key/value  " + key.toString() + "/" + value.toString());
+            tempkey++;
+            key = new LongWritable(tempkey);
+            writer.append(key,value);
+        }
+
+
+
+        writer.close();
+
+
+
+
+
+    }
+
+}
